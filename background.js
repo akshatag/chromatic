495c4423555bf23aa1167202adcbb50e3d75da8c
Akshat Agrawal <akshat@Akshats-MacBook-Pro.local>
2021-04-18 20:07:09 -0700

    seems to be working

--- background.js
+++ background.js
@@ -1,27 +1,77 @@
+tabShiftActive = false
+tabShiftActiveIdx = 0
 tabsMruOrder = []
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
-    // alert(tabsMruOrder.toString())
+resetTabShift = function() {
+    if(tabShiftActive) {
+        // alert("before" + tabsMruOrder.toString())
+        chrome.tabs.query({active: true}, (results) => {
+            activeTabId = results[0].id
+            activeTabMruIdx = tabsMruOrder.indexOf(activeTabId)
     
-    activeTabIdx = tabsMruOrder.indexOf(activeInfo.tabId)
+            if(activeTabMruIdx != -1){
+                tabsMruOrder.splice(activeTabMruIdx, 1)
+            }
+    
+            tabsMruOrder.unshift(activeTabId)
+            // alert("after" + tabsMruOrder.toString())
+            tabShiftActiveIdx = 0
+        })
 
-    if(activeTabIdx != -1) {
-        tabsMruOrder.splice(activeTabIdx, 1)
     }
 
-    tabsMruOrder.unshift(activeInfo.tabId)
+
+
+}
+
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    if(!tabShiftActive){
+        activeTabIdx = tabsMruOrder.indexOf(activeInfo.tabId)
+
+        if(activeTabIdx != -1) {
+            tabsMruOrder.splice(activeTabIdx, 1)
+        }
+    
+        tabsMruOrder.unshift(activeInfo.tabId)
+        // alert("tab" + tabsMruOrder.toString())
+    }
 })
 
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if(request.greeting == 'hello') {            
-            if(tabsMruOrder.length > 1) {
-                chrome.tabs.update(tabsMruOrder[1], {selected:true}, (tab) => {
-                    sendResponse({farewell: 'goodbye'})
+        if(request.route == 'META_KEYUP') {
+            if(tabShiftActive) {  
+                resetTabShift()
+            }
+        }
+    }
+)
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if(request.route == 'META_SHIFT_BACK') {            
+            tabShiftActive = true
+            if(tabShiftActiveIdx + 1 < tabsMruOrder.length) {
+                tabShiftActiveIdx += 1
+                chrome.tabs.update(tabsMruOrder[tabShiftActiveIdx], {selected:true}, (tab) => {
+                    sendResponse({});
+                }) 
+            }
+        }
+
+        if(request.route == 'META_SHIFT_FORWARD') {
+            tabShiftActive = true
+            if(tabShiftActiveIdx - 1 >= 0) {
+                tabShiftActiveIdx -= 1
+                chrome.tabs.update(tabsMruOrder[tabShiftActiveIdx], {selected:true}, (tab) => {
+                    sendResponse({});
                 }) 
             }    
         }
+
+        // chrome.extension.getBackgroundPage().console.log(tabsMruOrder.toString())
     }
 )
 
--- content.js
+++ content.js
@@ -1,12 +1,14 @@
 $(document).keydown(function(event){    
-    if(event.metaKey && event.key == 'Shift'){ 
-        chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-            console.log(response.farewell);
-          });
+    if(event.metaKey && event.key == 'Alt'){ 
+        chrome.runtime.sendMessage({route: "META_SHIFT_FORWARD"}, () => {})
+    }
+    if(event.metaKey && event.key == 'Control'){
+        chrome.runtime.sendMessage({route: "META_SHIFT_BACK"}, () => {})
     }
 })
 
-
-
-
-
+$(document).keyup(function(event){
+    if(event.key == 'Meta'){
+        chrome.runtime.sendMessage({route: "META_KEYUP"}, () => {})
+    }
+})
